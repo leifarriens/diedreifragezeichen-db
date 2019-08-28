a15acd3695bfbd88d6f8b289fc5807a671b45347
Leif Arriens <leif@arri-web.de>
2019-08-28 20:30:43 +0200

    api work

--- models/folge.js
+++ models/folge.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // Folge Schema
 const folgeSchema = mongoose.Schema({
   title: String,
-  number: Number,
-  ratings: Array
+  number: String,
+  ratings: Array,
+  rating: Number
 });
 
 const Folge = module.exports = mongoose.model('Folge', folgeSchema)
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+console.clear();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const scrapeIt = require('scrape-it');
 const app = express();
 
 const Folge = require('./models/folge');
@@ -23,26 +25,58 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const folgen = await Folge.find({}).sort('-rating');
+  console.log(folgen);
+  res.render('index', { folgen });
 });
 
-app.get('/folge/:id', async (req, res) => {
+app.get('/folge/:number', async (req, res) => {
   try {
-    const folge = await Folge.findById(req.params.id);
+    const folge = await Folge.findOne({ number: req.params.number });
+    console.log(folge);
     folge.rating = getRating(folge);
+
+    // const formattedTitle = folge.title.replace(/\s+/g, '-').toLowerCase();
+
+    // console.log(formattedTitle);
+
+    // scrapeIt(`https://dreifragezeichen.de/produktwelt/details/${formattedTitle}`, {
+    //   title: '.product-title',
+    //   release: '.title span:last-of-type',
+    //   inhalt: '#info-inhalt p',
+    //   sprecher: '#info-sprecher p'
+    // }).then(({ data, response }) => {
+    //   console.log(`Status Code: ${response.statusCode}`)
+    //   console.log(data)
+    //   folge.inhalt = data.inhalt;
+    //   folge.sprecher = data.sprecher;
+    //   folge.release = data.release;
+    //   res.render('folge', folge);
+    // })
+
     res.render('folge', folge);
   } catch (err) {
-    console.log(err);;
+    console.log(err);
+    res.render('404');
   }
+});
 
-  function getRating(folge) {
-    let s = 0;
-    folge.ratings.forEach(r => {
-      s = s + Number(r);
-    });
-    
-    return Math.floor(s / folge.ratings.length * 10) / 10;
+app.post('/folge/:number/rate', async (req, res) => {
+  try {
+    const folge = await Folge.findOne({ number: req.params.number });
+    console.log(folge);
+    folge.ratings.push(req.body.rating);
+    folge.rating = getRating(folge);
+
+    folge.save()
+    .then(() => {
+      res.redirect(`/folge/${req.params.number}`)
+    })
+    .catch(err => console.log(err));
+
+  } catch (err) {
+    console.log(err);
   }
 });
 
@@ -60,7 +94,17 @@ app.post('/folge', async (req, res) => {
     message: 'Folge gespeichert!'
   }))
   .catch(err => console.log(err));
-})
+});
+
+function getRating(folge) {
+  let s = 0;
+
+  folge.ratings.forEach(r => {
+    s = s + Number(r);
+  });
+  
+  return Math.floor(s / folge.ratings.length * 10) / 10;
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
