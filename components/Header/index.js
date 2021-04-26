56644c2fb1b37a41c1b928f9f0baec2e691b4ab2
Leif Arriens <leif.arriens@gmail.com>
2021-04-26 13:57:10 +0200

    dependency fix

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Headroom from 'react-headroom'
 import { Container, HomeLink, SearchBar, ProfileLink } from './StyledHeader'
 import { AiOutlineProfile } from 'react-icons/ai'
