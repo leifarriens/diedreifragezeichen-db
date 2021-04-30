bd07e67b94745150d50cfbf6ea95b1f3301b7ef1
Leif Arriens <leif.arriens@gmail.com>
2021-04-30 14:24:42 +0200

    model fix

--- services/index.js
+++ services/index.js
@@ -1,4 +1,5 @@
 import Folge from '../models/folge';
+import Rating from '../models/rating';
 
 export async function getFolgen() {
   return await Folge.find({}).sort('release_date').populate('ratings')
