bf9ebca1a5748750321844a4779375c94f766e8b
Leif Arriens <leif.arriens@gmail.com>
2021-05-28 04:10:19 +0200

    removed header from layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,8 @@
-import Header from './Header';
 import Footer from './Footer';
 
 function Layout({ children }) {
   return (
     <div className="container">
-      {/* <Header /> */}
       <main className="main">{children}</main>
       <Footer />
     </div>
