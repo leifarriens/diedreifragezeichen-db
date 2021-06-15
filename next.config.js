66f83fa9e9a7a88d7616549ad2d164904eec69b9
Leif Arriens <leif.arriens@gmail.com>
2021-06-15 23:44:19 +0200

    updated to nextJs 11

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
-  console.log(phase);
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
