d88e4444d2768a052d95b4b63fdb8d33327c2a01
Leif Arriens <leif.arriens@gmail.com>
2021-08-02 06:33:24 +0200

    toast component setup

---
+++ components/Toast.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 2rem;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const StlyedToast = styled.div`
+  z-index: 9999;
+  box-shadow: 0 0 20px 5px rgba(0,0,0,0.15);
+  background-color: #84E60B;
+  border-radius: 6px;
+  padding: 16px;
+  width: auto;
+`;
+
+function Toast({ children }) {
+  return (
+    <Container>
+      <StlyedToast>
+        {children}
+      </StlyedToast>
+    </Container>
+  )
+}
+
+export default Toast
