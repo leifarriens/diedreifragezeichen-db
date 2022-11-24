60ded3b2f1f9c842770f32bde14e3bef7077c739
Leif Arriens <leif.arriens@gmail.com>
2022-11-24 16:24:25 +0100

    pruned internal rest api endpoints for trpc

--- src/pages/api/folgen/[id]/index.ts
+++ src/pages/api/folgen/[id]/index.ts
@@ -2,8 +2,7 @@ import { Types } from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import dbConnect from '@/db/connect';
-import { getServerSession } from '@/lib/getServerSession';
-import { deleteFolge, getFolge, updateFolge } from '@/services/folge.service';
+import { getFolge } from '@/services/folge.service';
 import { parseQueryParam } from '@/utils/index';
 
 export default async function handler(
@@ -24,48 +23,6 @@ export default async function handler(
     if (!folge) return res.status(404).end('Not Found');
 
     return res.json(folge);
-  }
-
-  if (req.method === 'PATCH') {
-    const id = parseQueryParam(req.query.id);
-
-    if (!Types.ObjectId.isValid(id)) {
-      return res.status(404).end('Not Found');
-    }
-
-    const session = await getServerSession(req, res);
-
-    if (!session || session.user.role !== 'Admin') {
-      return res.status(403).end('Forbidden');
-    }
-
-    await dbConnect();
-
-    const folge = await updateFolge(id, req.body);
-
-    if (!folge) return res.status(404).end('Not Found');
-
-    return res.json(folge);
-  }
-
-  if (req.method === 'DELETE') {
-    const id = parseQueryParam(req.query.id);
-
-    if (!Types.ObjectId.isValid(id)) {
-      return res.status(404).end('Not Found');
-    }
-
-    const session = await getServerSession(req, res);
-
-    if (!session || session.user.role !== 'Admin') {
-      return res.status(403).end('Forbidden');
-    }
-
-    await dbConnect();
-
-    await deleteFolge(id);
-
-    return res.status(204).end();
   }
 
   return res.status(405).end('Method not allowed');
