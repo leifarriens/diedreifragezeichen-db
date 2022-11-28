c5f5802e8bbe12e2c4b26dc5654936d432cec1ed
Leif Arriens <leif.arriens@gmail.com>
2022-11-28 18:32:23 +0100

    add: basic local playwright testing

---
+++ tests/app.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from '@playwright/test';
+
+test('homepage has title and links to datenschutz page', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  await expect(page).toHaveTitle(/Drei Fragezeichen DB/);
+
+  const dsgLink = page.getByRole('link', { name: 'Datenschutz' });
+
+  await expect(dsgLink).toHaveAttribute('href', '/datenschutz');
+
+  await dsgLink.click();
+
+  await expect(page).toHaveURL(/.*datenschutz/);
+});
