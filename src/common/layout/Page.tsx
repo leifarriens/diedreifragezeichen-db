74d6870befdaa7c2c33ef8f4dedf97e6e58b9ed1
Leif Arriens <leif.arriens@gmail.com>
2022-12-20 08:33:32 +0100

    fix: broken page container flex

--- src/common/layout/Page.tsx
+++ src/common/layout/Page.tsx
@@ -10,7 +10,7 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="container">
       <Header />
       <main className="flex-1 grid place-items-center items-center justify-items-center">
         {children}
