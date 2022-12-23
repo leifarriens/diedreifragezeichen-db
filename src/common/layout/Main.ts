c4eadca5f7b25468074a9794a6667311ca8c07ac
Leif Arriens <leif.arriens@gmail.com>
2022-12-23 08:54:32 +0100

    fix: main layout

---
+++ src/common/layout/Main.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+  flex: 1;
+  display: grid;
+  place-items: center;
+  align-items: stretch;
+  justify-items: stretch;
+`;
+
+export default Main;
--- src/common/layout/Page.tsx
+++ src/common/layout/Page.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import ScrollToTop from '@/components/ScrollToTop';
+import Main from '@/layout/Main';
 
 type LayoutProps = {
   children: ReactNode | ReactNode[];
@@ -12,9 +13,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="container">
       <Header />
-      <main className="flex-1 grid place-items-center items-center justify-items-center">
-        {children}
-      </main>
+      <Main className="flex-1 ">{children}</Main>
       <Footer />
       <ScrollToTop />
     </div>
