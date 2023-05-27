8f8b5c6063f4a3b13a0e3e5582f96818a8d9d89b
Leif Arriens <leif.arriens@gmail.com>
2023-05-27 15:54:17 +0200

    test: add grid filter test

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -13,3 +13,20 @@ test('homepage has title and links to datenschutz page', async ({ page }) => {
 
   await expect(page).toHaveURL(/.*datenschutz/);
 });
+
+// DISCUSS: this could fail test on first deploy before an initial sync
+test('folgen can be filtered and links to folge', async ({ page }) => {
+  await page.goto('/');
+
+  const searchInput = page.getByPlaceholder(
+    'Name, Nummer oder Erscheinungsjahr',
+  );
+
+  await searchInput.fill('feuermond');
+
+  const folge = page.getByRole('article').first();
+
+  await folge.getByRole('link').click();
+
+  await expect(page).toHaveTitle('Drei Fragezeichen DB | Folge 125 Feuermond');
+});
