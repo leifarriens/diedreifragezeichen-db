6dc620a396f390a52b8668a9aff1786921e967cc
Leif Arriens <leif.arriens@gmail.com>
2023-07-26 00:00:39 +0200

    feat: user ratings sortable

--- src/pages/admin/folgen/index.tsx
+++ src/pages/admin/folgen/index.tsx
@@ -4,21 +4,19 @@ import React, { useState } from 'react';
 import toast from 'react-hot-toast';
 import { FaDeezer, FaRegEyeSlash, FaSpotify, FaSyncAlt } from 'react-icons/fa';
 import { InView } from 'react-intersection-observer';
-import type { z } from 'zod';
 
 import { Button, Loader, Select, Switch } from '@/components/shared';
-import { AllFolgenSortOptions } from '@/constants/enums';
+import type { AllFolgenSortOptions } from '@/constants/enums';
+import { allFolgenSortOptions } from '@/constants/enums';
 import { DATE_FORMAT } from '@/constants/formats';
 import dayjs from '@/lib/dayjs';
 import { getServerAuthSesion } from '@/lib/getServerAuthSesion';
 import { useGridState } from '@/modules/Grid';
 import { type RouterOutput, trpc } from '@/utils/trpc';
 
-type SortOptions = z.infer<typeof AllFolgenSortOptions>;
-
 const AdminFolgen: NextPage = () => {
   const { searchQuery, showSpecials, setShowSpecials } = useGridState();
-  const [sort, setSort] = useState<SortOptions>();
+  const [sort, setSort] = useState<AllFolgenSortOptions>();
   const {
     data,
     refetch,
@@ -42,7 +40,7 @@ const AdminFolgen: NextPage = () => {
   function handleSortChange(e: React.ChangeEvent<HTMLSelectElement>) {
     if (!e.target.value) return;
 
-    const value = e.target.value as SortOptions;
+    const value = e.target.value as AllFolgenSortOptions;
 
     setSort(value);
   }
@@ -52,13 +50,11 @@ const AdminFolgen: NextPage = () => {
       <div className="mb-12 flex items-center justify-between">
         <div className="flex items-center gap-x-4">
           <Select value={sort} onChange={handleSortChange}>
-            {Object.keys(AllFolgenSortOptions.removeDefault().Values).map(
-              (value) => (
-                <option key={value} value={value}>
-                  {value}
-                </option>
-              ),
-            )}
+            {allFolgenSortOptions.map((value) => (
+              <option key={value} value={value}>
+                {value}
+              </option>
+            ))}
           </Select>
           <Switch
             checked={showSpecials}
