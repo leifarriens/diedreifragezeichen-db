2d535373bf707cbaf985eccad8f01a81457166f1
Leif Arriens <leif.arriens@gmail.com>
2023-08-07 18:31:55 +0200

    feat: update list cache on actions

--- src/common/hooks/useUserList.ts
+++ src/common/hooks/useUserList.ts
@@ -2,10 +2,60 @@ import { useSession } from 'next-auth/react';
 
 import { trpc } from '@/utils/trpc';
 
+const staleTime = 5 * 1000 * 60;
+const cacheTime = 5 * 1000 * 60;
+
 export function useUserList() {
   const { status } = useSession();
 
   return trpc.list.all.useQuery(undefined, {
     enabled: status === 'authenticated',
+    staleTime,
+    cacheTime,
   });
+}
+
+export function useUserListFolgen({
+  queryEnabled,
+}: { queryEnabled?: boolean } = {}) {
+  const { status } = useSession();
+  const utils = trpc.useContext();
+
+  const limit = 20;
+
+  return {
+    query: trpc.list.allFolgen.useInfiniteQuery(
+      { limit },
+      {
+        getNextPageParam: (lastPage) => {
+          if (lastPage.offset + lastPage.limit < lastPage.total) {
+            return lastPage.offset + lastPage.limit;
+          }
+          return undefined;
+        },
+        staleTime,
+        cacheTime,
+        enabled: queryEnabled && status === 'authenticated',
+      },
+    ),
+    setDataRemoveFolge: (folgeId: string) => {
+      utils.list.allFolgen.setInfiniteData({ limit }, (data) => {
+        if (!data) {
+          return {
+            pages: [],
+            pageParams: [],
+          };
+        }
+        return {
+          ...data,
+          pages: data.pages.map((page) => ({
+            ...page,
+            items: page.items.filter(
+              (folge) => folge._id.toString() !== folgeId,
+            ),
+          })),
+        };
+      });
+    },
+  };
 }
