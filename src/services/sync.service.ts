3cc6d1a9281303457cb1066c206b4b898167882a
Leif Arriens <leif.arriens@gmail.com>
2023-09-10 03:15:17 +0200

    feat: migrate sync to details service

--- src/services/sync.service.ts
+++ src/services/sync.service.ts
@@ -5,7 +5,7 @@ import { Folge as FolgeModel } from '@/models/folge';
 import { convertFolge } from '@/utils/convertFolge';
 
 import ignorelist from '../../config/ignorelist.json';
-import { getAllInhalte } from './inhalt.service';
+import { getAllWeblinks, getFolgenDetails } from './details.service';
 import * as DeezerApi from './streaming/deezer';
 import * as SpotifyApi from './streaming/spotify';
 
@@ -41,43 +41,6 @@ export async function syncFolgen() {
 }
 
 /**
- * The `syncInhalte` service updates all folgen without a `inhalt`
- * with the found `body` from the `getAllInhalte` service.
- */
-export async function syncInhalte() {
-  const inhalte = await getAllInhalte();
-
-  const folgen = await FolgeModel.find({});
-
-  const writes = folgen
-    .filter((folge) => !folge.inhalt)
-    .reduce<AnyBulkWriteOperation[]>((curr, folge) => {
-      const inhtaltExp = folge.name
-        .replace('und', '')
-        .replace('???', '')
-        .trim();
-      const inhalt = inhalte.find(({ name }) =>
-        new RegExp(inhtaltExp, 'i').test(name),
-      );
-
-      if (inhalt) {
-        curr.push({
-          updateOne: {
-            filter: { _id: new ObjectId(folge._id) },
-            update: { inhalt: inhalt.body },
-          },
-        });
-      }
-
-      return curr;
-    }, []);
-
-  const result = await FolgeModel.bulkWrite(writes);
-
-  return { result, writes };
-}
-
-/**
  * The `syncDezzer` service updates all folgen without a `deezer_id`
  * with the found `deezerAlbum.id` from the deezer api.
  */
@@ -110,4 +73,111 @@ export async function syncDeezer() {
   const result = await FolgeModel.bulkWrite(writes);
 
   return { result, writes };
+}
+
+/**
+ * The `syncWeblinks` service updates all folgen without a `weblink`
+ * with the found `link` from the `getAllWeblinks` service.
+ */
+export async function syncWeblinks() {
+  const weblinks = await getAllWeblinks();
+
+  const folgen = await FolgeModel.find({});
+
+  const writes = folgen
+    .filter((folge) => !folge.weblink)
+    .reduce<AnyBulkWriteOperation[]>((curr, folge) => {
+      const exp = folge.name.replace('und', '').replace('???', '').trim();
+      const weblink = weblinks.find(({ title }) =>
+        new RegExp(exp, 'i').test(title),
+      );
+
+      if (weblink) {
+        curr.push({
+          updateOne: {
+            filter: { _id: new ObjectId(folge._id) },
+            update: { weblink: weblink.url },
+          },
+        });
+      }
+
+      return curr;
+    }, []);
+
+  const result = await FolgeModel.bulkWrite(writes);
+
+  return { result, writes };
+}
+
+/**
+ * The `syncFolgenDetails` service updates a folge by id with the found `inhalt` &
+ * `sprecher` from the `getFolgenDetails` service.
+ */
+export async function syncFolgenDetails(folgeId: string) {
+  const folge = await FolgeModel.findById(folgeId);
+
+  if (!folge || !folge.weblink) {
+    return null;
+  }
+
+  const details = await getFolgenDetails(folge.weblink);
+
+  if (!folge.inhalt) {
+    folge.inhalt = details.inhalt;
+  }
+
+  if (!folge.sprecher) {
+    folge.sprecher = details.sprecher;
+  }
+
+  await folge.save();
+
+  return folge;
+}
+
+/**
+ * The `syncDetails` service updates all non-hidden folgen with a `weblink` and
+ * without `inhalt` or `sprecher` from the `getFolgenDetails` service.
+ */
+export async function syncDetails() {
+  let folgen = await FolgeModel.find({
+    $or: [{ isHidden: false }, { isHidden: { $exists: false } }],
+  });
+
+  folgen = folgen.filter(
+    (folge) => folge.weblink && (!folge.inhalt || !folge.sprecher),
+  );
+
+  const weblinks = folgen
+    .map((folge) => folge.weblink)
+    .filter((url): url is string => !!url);
+
+  const details = await Promise.all(
+    weblinks.map((url) => getFolgenDetails(url)),
+  );
+
+  const writes = folgen.reduce<AnyBulkWriteOperation[]>((curr, folge) => {
+    const exp = folge.name.replace('und', '').replace('???', '').trim();
+    const detail = details.find(({ title }) =>
+      new RegExp(exp, 'i').test(title),
+    );
+
+    if (detail) {
+      curr.push({
+        updateOne: {
+          filter: { _id: new ObjectId(folge._id) },
+          update: {
+            ...(!folge.inhalt && { inhalt: detail.inhalt }),
+            ...(!folge.sprecher && { sprecher: detail.sprecher }),
+          },
+        },
+      });
+    }
+
+    return curr;
+  }, []);
+
+  const result = await FolgeModel.bulkWrite(writes);
+
+  return { result, writes, details };
 }
