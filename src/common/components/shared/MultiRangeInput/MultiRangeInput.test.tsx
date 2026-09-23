0da4ece1801be9db6621c123e711b26a895e9401
Leif Arriens <leif.arriens@gmail.com>
2026-09-23 11:05:58 +0200

    chore: update dependencies

--- src/common/components/shared/MultiRangeInput/MultiRangeInput.test.tsx
+++ src/common/components/shared/MultiRangeInput/MultiRangeInput.test.tsx
@@ -1,20 +1,30 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
-import { describe, expect, test, vi } from 'vitest';
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 
 import { MultiRangeInput } from './MultiRangeInput';
 
-describe('MultiRangeInput', () => {
-  const min = 1;
-  const max = 100;
+const min = 1;
+const max = 100;
+
+const setup = () => {
   const onChange = vi.fn();
   const { getByLabelText } = render(
     <MultiRangeInput min={min} max={max} onChange={onChange} />,
   );
 
-  const minSlider = getByLabelText('Slider Minimum') as HTMLInputElement;
-  const maxSlider = getByLabelText('Slider Maximum') as HTMLInputElement;
+  return {
+    minSlider: getByLabelText('Slider Minimum') as HTMLInputElement,
+    maxSlider: getByLabelText('Slider Maximum') as HTMLInputElement,
+    onChange,
+  };
+};
+
+describe('MultiRangeInput', () => {
+  afterEach(cleanup);
 
   test('sets min and max value correctly', () => {
+    const { minSlider, maxSlider } = setup();
+
     fireEvent.change(minSlider, { target: { value: '20' } });
     fireEvent.change(maxSlider, { target: { value: '70' } });
 
@@ -23,6 +33,9 @@ describe('MultiRangeInput', () => {
   });
 
   test('min cannot be set higher than max', async () => {
+    const { minSlider, maxSlider, onChange } = setup();
+
+    fireEvent.change(maxSlider, { target: { value: '70' } });
     fireEvent.change(minSlider, { target: { value: '80' } });
 
     // callback is debounced
@@ -35,6 +48,8 @@ describe('MultiRangeInput', () => {
   });
 
   test('max cannot be set lower than min', async () => {
+    const { minSlider, maxSlider, onChange } = setup();
+
     fireEvent.change(minSlider, { target: { value: '50' } });
     fireEvent.change(maxSlider, { target: { value: '40' } });
 
@@ -48,8 +63,10 @@ describe('MultiRangeInput', () => {
   });
 
   test('Slider Minimum cannot be set lower than min prop', async () => {
+    const { minSlider, onChange } = setup();
+
+    fireEvent.change(minSlider, { target: { value: '50' } });
     fireEvent.change(minSlider, { target: { value: '0' } });
-    fireEvent.change(maxSlider, { target: { value: max } });
 
     // callback is debounced
     await waitFor(
@@ -61,7 +78,9 @@ describe('MultiRangeInput', () => {
   });
 
   test('Slider Maximum cannot be set higher than max prop', async () => {
-    fireEvent.change(minSlider, { target: { value: min } });
+    const { maxSlider, onChange } = setup();
+
+    fireEvent.change(maxSlider, { target: { value: '70' } });
     fireEvent.change(maxSlider, { target: { value: max + 1 } });
 
     // callback is debounced
